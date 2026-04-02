f263d71ee9b767b126dd7ac221acbbe0937b382d
Rhezna <99413088+Rhezna@users.noreply.github.com>
2026-04-03 01:13:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -105,4 +105,18 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000);
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
